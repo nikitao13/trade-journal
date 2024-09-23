7917a30de1ecd9586d78eed27f2da7cc58d7b700
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-09-23 14:08:07 +1000

    added support for multiple currencies

---
+++ cryptoPrice.js
@@ -0,0 +1,18 @@
+export default async function getCryptoPrice() {
+  const currency = process.env.CURRENCY.toLowerCase();
+  const crypto = process.env.CRYPTO.toLowerCase();
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=${currency}`;
+  
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`[${crypto} ${currency}]`);
+    return data[crypto][currency];
+  } catch (error) {
+    console.error(`Error fetching ${crypto} price:`, error);
+    return null;
+  }
+}
